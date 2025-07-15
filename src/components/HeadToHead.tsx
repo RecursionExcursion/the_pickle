3a27ba091e5b9280e4657cb58ce840e3cddaf05c
RecursionExcursion <rloup15@gmail.com>
2025-07-15 12:19:52 -0400

    added gap in h2h

--- src/components/HeadToHead.tsx
+++ src/components/HeadToHead.tsx
@@ -43,7 +43,7 @@ export default function HeadToHead() {
         Add Match
       </LinkButton>
       <h2>Head To Head</h2>
-      <div className="flex">
+      <div className="flex gap-2">
         <PlayerSelect
           playerName={player1Id}
           setPlayerName={setPlayer1Id}
